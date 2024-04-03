8356e2e2ac4994a62c24a699cd5c87cc3cae883e
David Salter <davidmsalter@me.com>
2024-04-03 21:56:00 +0100

    Add border to documentation and diagnostic popups

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -11,3 +11,17 @@ vim.opt.signcolumn = "number"
 
 vim.opt.conceallevel = 2
 vim.opt.backup = false
+
+local _border = "single"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = _border,
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	border = _border,
+})
+
+vim.diagnostic.config({
+	float = { border = _border },
+})
