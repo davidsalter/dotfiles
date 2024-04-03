4d3c9dc5e1c8166e2c2cc39c42da6fcc51365d31
David Salter <davidmsalter@me.com>
2024-04-03 22:08:50 +0100

    Refactored java to activate on each appropriate buffer

---
+++ nvim/ftplugin/java.lua
@@ -0,0 +1,36 @@
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = "/home/david/.workspace/" .. project_name
+local config = {
+	cmd = {
+		"java",
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-Xmx1g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
+    "-javaagent:/home/david/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+		"-jar",
+		vim.fn.glob("/home/david/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+		"-configuration",
+		"/home/david/.local/share/nvim/mason/packages/jdtls/config_linux",
+		"-data",
+		workspace_dir,
+	},
+	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+	settings = {
+		java = {},
+	},
+	init_options = {
+		bundles = {},
+	},
+}
+vim.keymap.set("n", "<Leader>cec", require("jdtls").extract_constant, {})
+vim.keymap.set("n", "<Leader>cev", require("jdtls").extract_variable, {})
+vim.keymap.set("n", "<Leader>coi", require("jdtls").organize_imports, {})
+require("jdtls").start_or_attach(config)
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -24,6 +24,7 @@ return {
         defaults = {
           file_ignore_patterns = {
             "bin/*",
+            "**/*.class",
           },
         },
       })
